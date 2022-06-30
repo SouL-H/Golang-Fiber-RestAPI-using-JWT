27bff7f829bed763a317aebbcb01781af3efbbdd
SouL-H <mhmtaydinw@gmail.com>
2022-06-30 16:11:02 +0300

    env added and db connected

---
+++ db/conn.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"go_jwt_samplev2/models"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Connect() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	a := os.Getenv("dbConn")
+
+	conn, err := gorm.Open(mysql.Open(a), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+	conn.AutoMigrate(&models.User{}) //Auto created
+}
